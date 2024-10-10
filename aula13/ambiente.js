f0d1e287ede6b50f512736f7ba7c9923d9be43b5
Bruno Simon <brusife19@gmail.com>
2024-10-10 16:36:59 -0300

    estudos

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+/*var c = 1
+while(c<=6) {
+    console.log (`Passo ${c}`)
+    c++
+}*/
+/*var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
+*/
+
+var x = 10
+while (x>=0){
+    console.log (`Contando ... ${x}`)
+    x--
+}
---
+++ aula14/node_9fe3846e36196.tmp.js
@@ -0,0 +1,3 @@
+for (var c=1;c<=99887454;c++){
+    console.log(c)
+}
